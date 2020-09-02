3b7121eb73cea25896ae5e909362404e071081f8
claudiu.nedelcu <claudiu.nedelcu@TS1305>
2020-09-02 09:25:46 +0300

    commit?

--- ConferencePlanner/ConferencePlanner.RepositoryEf/Repository/GetDemoRepository.cs
+++ ConferencePlanner/ConferencePlanner.RepositoryEf/Repository/GetDemoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ConferencePlanner.Abstraction.Model;
 using ConferencePlanner.Abstraction.Repository;
 using ConferencePlanner.Repository.Ef.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +23,12 @@ namespace ConferencePlanner.Repository.Ef.Repository
         public List<DemoModel> GetDemo(string name)
         {
             //List<Demo> demos = _dbContext.Demo.ToList();
-            List<Conference> conferences = _untoldContext.Conference.ToList();
+            List<Conference> conferences = _untoldContext.Conference.Include(x=>x.ConferenceType).Include(x=>x.ConferenceCategory).ToList();
+            //Conference conference = _untoldContext.Conference.FirstOrDefault(x=>x.ConferenceName=="test");
+            //conferences.Add(conference);
 
-            List<DemoModel> demoModels = conferences.Select(a => new DemoModel() { Id = a.ConferenceId, Name = a.ConferenceName }).ToList();
+            //List<DemoModel> demoModels = conferences.Select(a => new DemoModel() {  Id = a.ConferenceId, Name = a.ConferenceName }).ToList();
+            List<DemoModel> demoModels = conferences.Select(a => new DemoModel() { Id = a.ConferenceId, Name = a.ConferenceType.ConferenceTypeName }).ToList();
 
             return demoModels;
         }
