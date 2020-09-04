db05a1854c4d1fbb598ad01ba741404058a299bc
Adrian Munteanu <adrian.munteanu@totalsoft.com>
2020-09-04 15:38:46 +0300

    bagul ala mare rezolvat

--- ConferencePlanner/ConferencePlanner.RepositoryEf/Entities/DictionaryCountry.cs
+++ ConferencePlanner/ConferencePlanner.RepositoryEf/Entities/DictionaryCountry.cs
@@ -13,11 +13,7 @@ namespace ConferencePlanner.Repository.Ef.Entities
         public int DictionaryCountryId { get; set; }
         public string CountryName { get; set; }
         public string CountryCode { get; set; }
-        public virtual DictionaryCity City { get; set; }
         public virtual ICollection<DictionaryCounty> DictionaryCounty { get; set; }
 
-        public virtual Location Location { get; set; }
-
-        public virtual DictionaryCounty County { get; set; }
     }
 }
